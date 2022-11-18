88c86145d7d268f6dea4ac4c956f56372868ca67
Luka Souza <lukasouza77@gmail.com>
2022-11-18 19:48:53 -0300

    -

--- js/burger.js
+++ js/burger.js
@@ -1,15 +1,15 @@
-const burger = document.getElementById('toggler');
-const mobile = document.getElementById('mobile');
-const algum = document.querySelectorAll("#algum1, #algum2, #algum3, #algum4")
+const $burger = document.getElementById('toggler');
+const $mobile = document.getElementById('mobile');
+const $algum = document.querySelectorAll("#algum1, #algum2, #algum3, #algum4");
 
-burger.addEventListener('click', toggleClass)
-algum[0].addEventListener('click', close)
-algum[1].addEventListener('click', close)
+$burger.addEventListener('click', () => {toggleClass()})
+$algum[0].addEventListener('click', () => {close()})
+$algum[1].addEventListener('click', () => {close()})
 
-function toggleClass(){
-    mobile.classList.toggle('toggle');
+const toggleClass = () => {
+    $mobile.classList.toggle('toggle');
 }
 
-function close(){
-    burger.click();
+const close = () => {
+    $burger.click();
 }
--- js/currency.js
+++ js/currency.js
@@ -1,73 +1,73 @@
-const input = document.getElementById('trocar-moeda');
-const moedap = document.getElementById('moeda-mes');
-const precop = document.getElementById('preco-mes');
-const moedam = document.getElementById('moeda-vida');
-const precom = document.getElementById('preco-vida');
-const moedae = document.getElementById('moeda-emp');
-const precoe = document.getElementById('preco-emp');
+const $input = document.getElementById('trocar-moeda');
+const $moedap = document.getElementById('moeda-mes');
+const $precop = document.getElementById('preco-mes');
+const $moedam = document.getElementById('moeda-vida');
+const $precom = document.getElementById('preco-vida');
+const $moedae = document.getElementById('moeda-emp');
+const $precoe = document.getElementById('preco-emp');
 
-input.onchange = trocando;
-  
-function trocando(){
+$input.addEventListener('change', trocando)
+
+function trocando() {
   let value = this.value;
-    
+
     if (value == "1"){
       reais();
     }else if (value == "2"){
       dollar();
     } else {
-        alert('[ERRO] Desconhecido.');
+      alert('[ERRO] Desconhecido.');
     }
 }
 
-function reais() {
-    precop.innerHTML = '15';
-    moedap.innerHTML = 'R$'
+const reais = () => {
+    $precop.innerHTML = '15';
+    $moedap.innerHTML = 'R$'
 
-    moedap.classList.add('moeda');
-    moedap.classList.remove('valor');
-    precop.classList.add('valor');
-    precop.classList.remove('moeda');
+    $moedap.classList.add('moeda');
+    $moedap.classList.remove('valor');
+    $precop.classList.add('valor');
+    $precop.classList.remove('moeda');
 
-    precom.innerHTML = '50';
-    moedam.innerHTML = 'R$'
+    $precom.innerHTML = '50';
+    $moedam.innerHTML = 'R$'
 
-    moedam.classList.add('moeda');
-    moedam.classList.remove('valor');
-    precom.classList.add('valor');
-    precom.classList.remove('moeda');
+    $moedam.classList.add('moeda');
+    $moedam.classList.remove('valor');
+    $precom.classList.add('valor');
+    $precom.classList.remove('moeda');
 
-    precoe.innerHTML = '70';
-    moedae.innerHTML = 'R$'
+    $precoe.innerHTML = '70';
+    $moedae.innerHTML = 'R$'
 
-    moedae.classList.add('moeda');
-    moedae.classList.remove('valor');
-    precoe.classList.add('valor');
-    precoe.classList.remove('moeda');
+    $moedae.classList.add('moeda');
+    $moedae.classList.remove('valor');
+    $precoe.classList.add('valor');
+    $precoe.classList.remove('moeda');
 }
 
-function dollar() {
-    precop.innerHTML = '$';
-    moedap.innerHTML = '5'
+const dollar = () => {
+    $precop.innerHTML = '$';
+    $moedap.innerHTML = '5'
 
-    moedap.classList.remove('moeda');
-    moedap.classList.add('valor');
-    precop.classList.remove('valor');
-    precop.classList.add('moeda');
+    $moedap.classList.remove('moeda');
+    $moedap.classList.add('valor');
+    $precop.classList.remove('valor');
+    $precop.classList.add('moeda');
 
-    precom.innerHTML = '$';
-    moedam.innerHTML = '10'
+    $precom.innerHTML = '$';
+    $moedam.innerHTML = '10'
 
-    moedam.classList.remove('moeda');
-    moedam.classList.add('valor');
-    precom.classList.remove('valor');
-    precom.classList.add('moeda');
+    $moedam.classList.remove('moeda');
+    $moedam.classList.add('valor');
+    $precom.classList.remove('valor');
+    $precom.classList.add('moeda');
 
-    precoe.innerHTML = '$';
-    moedae.innerHTML = '14'
+    $precoe.innerHTML = '$';
+    $moedae.innerHTML = '14'
 
-    moedae.classList.remove('moeda');
-    moedae.classList.add('valor');
-    precoe.classList.remove('valor');
-    precoe.classList.add('moeda');
+    $moedae.classList.remove('moeda');
+    $moedae.classList.add('valor');
+    $precoe.classList.remove('valor');
+    $precoe.classList.add('moeda');
 }
