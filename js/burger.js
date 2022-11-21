ed79f4e472c99112c8f5a70b5974512b3db0256a
Luka Souza <lukasouza77@gmail.com>
2022-11-21 01:37:23 -0300

    -

--- js/burger.js
+++ js/burger.js
@@ -1,15 +1,10 @@
 const $burger = document.getElementById('toggler');
 const $mobile = document.getElementById('mobile');
-const $algum = document.querySelectorAll("#algum1, #algum2, #algum3, #algum4");
+const $allButtons = document.querySelectorAll('.algum')
 
 $burger.addEventListener('click', () => {toggleClass()})
-$algum[0].addEventListener('click', () => {close()})
-$algum[1].addEventListener('click', () => {close()})
+$allButtons.forEach(buttons => {buttons.addEventListener('click', () => {close()})})
 
-const toggleClass = () => {
-    $mobile.classList.toggle('toggle');
-}
+const toggleClass = () => {$mobile.classList.toggle('toggle')}
 
-const close = () => {
-    $burger.click();
-}
+const close = () => {$burger.click()}
