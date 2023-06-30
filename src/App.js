457705831c426a932bcda997e47e0be2dfe8049d
Luka Souza <lukasouza77@gmail.com>
2023-06-30 02:19:35 -0300

    '.'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header.js';
 import Home from './components/home.js';
+import Partners from './components/partners.js';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Partners />
     </div>
   );
 }
---
+++ src/components/partners.js
@@ -0,0 +1,20 @@
+import '../style/partners.css';
+
+export default function Partners() {
+
+    return (
+        <section>
+            <div className="partners">
+                <h1>PARTNERS</h1>
+                <div className="partners-showcase">
+                    <a href="#link1" aria-label="Logo parceria 1"><img src="https://cdn.discordapp.com/attachments/890745869832454145/1124174161179328534/genericimg.png" alt="" /></a>
+                    <a href="#link2" aria-label="Logo parceria 2"><img src="https://cdn.discordapp.com/attachments/890745869832454145/1124174161179328534/genericimg.png" alt="" /></a>
+                    <a href="#link3" aria-label="Logo parceria 3"><img src="https://cdn.discordapp.com/attachments/890745869832454145/1124174161179328534/genericimg.png" alt="" /></a>
+                    <a href="#link4" aria-label="Logo parceria 3"><img src="https://cdn.discordapp.com/attachments/890745869832454145/1124174161179328534/genericimg.png" alt="" /></a>
+                    <a href="#link5" aria-label="Logo parceria 3"><img src="https://cdn.discordapp.com/attachments/890745869832454145/1124174161179328534/genericimg.png" alt="" /></a>
+                </div>
+                <p>Talk with some owner or director if you want to become a partner.</p>
+            </div>
+        </section>
+    )
+}
