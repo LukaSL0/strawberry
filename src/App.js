cc8eb1376db4764495be7a3011262b17800a7ca2
Luka Souza <lukasouza77@gmail.com>
2023-06-30 00:07:05 -0300

    '.'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header.js';
+import Home from './components/home.js';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import '../style/home.css';
+
+export default function Home() {
+    return (
+        <section>
+            <div className="home">
+                <img src="https://media.discordapp.net/attachments/890745869832454145/1124174159828754512/strawberry.png" alt="" />
+                <div className="home-txt">
+                    <h1>Be the good for the community 🔍</h1>
+                    <a href="#pricing">PURCHASE</a>
+                </div>
+            </div>
+        </section>
+    )
+}
