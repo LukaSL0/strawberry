ad1dcef9266853fd6547836caa71c74c863204bf
Luka Souza <lukasouza77@gmail.com>
2023-06-29 23:49:45 -0300

    '.'

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,32 @@
 import '../style/header.css';
+import { useRef } from "react";
 
 export default function Header() {
 
+    const mobile = useRef();
+    const toggle = () => {
+        mobile.current.classList.toggle("toggle");
+    }
+
     return (
-        <div></div>
+        <header>
+            <h1>STRAWBERRY</h1>
+            <div className="burgericon">
+                <div className="burgeroptns" ref={mobile}>
+                <li><a href="#home" onClick={() => {toggle()}}>Home</a></li>
+                    <li><a href="#features" onClick={() => {toggle()}}>Features</a></li>
+                    <li><a href="#pricing" onClick={() => {toggle()}}>Pricing</a></li>
+                </div>
+                <input type="checkbox" className="toggler" onClick={() => {toggle()}} aria-label="Burger" />
+                <div className="hamburger"><div></div></div>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#features">Features</a></li>
+                    <li><a href="#pricing">Pricing</a></li>
+                    <li className="discordbtn"><a href="#discord-link">Discord</a></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
